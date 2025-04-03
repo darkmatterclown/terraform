df779a2df24b135b736e63b063ae2a9887cfbfd5
harris-97 <harrisnetto@gmail.com>
2025-04-03 11:42:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"  # Change as needed
+}
+
+# Create IAM User
+
+resource "aws_iam_user" "terraform_user" {
+  name = "terraform-user"
+}
+
