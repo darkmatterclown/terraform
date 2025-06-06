949126236512f08042e762d187f4ad51a8cf23b6
Harris Netto <harrisnetto@gmail.com>
2025-06-06 12:54:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,9 +7,17 @@ terraform {
   }
 }
 provider "aws" {
-  region = "us-east-1"  # Change as needed
+  region = "us-east-1" # Change as needed
 }
 
+terraform {
+  backend "s3" {
+    bucket = "jenkins-terraform-state-7297"
+    key    = "tfstate"
+    region = "us-east-1"
+  }
+
+}
 # Create IAM User
 
 resource "aws_iam_user" "terraform_user" {
